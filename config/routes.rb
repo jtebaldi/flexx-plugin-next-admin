1eefc2111e8516d9355ba50501557c92733de87b
Paulo Henrique Castro <phlcastro@gmail.com>
2018-05-23 19:06:21 +1000

    moved recipe ui to flexx-plugin-crm

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
       #Admin Panel
       scope :admin, as: 'admin', path: PluginRoutes.system_info['admin_path_name'] do
         namespace :next do
-          resources :recipes
           resources :tasks
           resources :campaigns
           resources :dashboard
