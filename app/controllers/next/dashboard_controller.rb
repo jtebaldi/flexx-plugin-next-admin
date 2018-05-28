3615ae282a7cddf973059fb90523091acecea470
Joe Tebaldi <joetebaldi@gmail.com>
2018-05-27 23:54:12 -0400

    Updated dashboard

--- app/controllers/next/dashboard_controller.rb
+++ app/controllers/next/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Next::DashboardController < CamaleonCms::CamaleonController
   layout "flexx_next_admin"
 
   def index
+    @active_contacts = current_site.contacts.active
   end
 
   def settings
